86d7af9db5b7dc4352e118b9a6440909dbc27fd9
Sami-20s <samikfouri22@gmail.com>
2021-09-02 00:49:03 +0300

    transition effect

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-
+let mobile = document.querySelector(".mobile2__container");
+let shop = document.querySelector(".features__container__shop");
+let message = document.querySelector(".features__container__message");
+let paint = document.querySelector(".features__container__paint");
+let phone = document.querySelector(".features__container__phone");
+window.onscroll = function () {
+    console.log(this.scrollY);
+    if (this.scrollY >= 1000) {
+        mobile.classList.add("f");
+        shop.classList.add("shopm");
+        message.classList.add("messagem");
+        paint.classList.add("paintm");
+        phone.classList.add("phonem");
+    } else {
+        mobile.classList.remove("f");
+        shop.classList.remove("shopm");
+        message.classList.remove("messagem");
+        paint.classList.remove("paintm");
+        phone.classList.remove("phonem");
+    }
+};
